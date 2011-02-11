10bf4157d51d45c3f93c6d6e7217912832979a47
Alex Lee <lee@ccs.neu.edu>
2011-02-11 13:32:45 -0500

    initial commit

---
+++ main.rkt
@@ -0,0 +1,81 @@
+; -*- mode: scheme -*-
+#lang racket
+(provide (all-defined-out))
+
+(define init-env (hasheq))
+
+(struct value ())
+
+(define (extend x v env)
+  (hash-set env x (cons v (hash-ref env x '()))))
+
+(define (extend-list xs vs env)
+  (foldl extend
+         env
+         xs
+         vs))
+
+(define (get x env)
+  (let ((values (hash-ref env x)))
+    (if (empty? values)
+        (error 'get "no mapping for ~a" x)
+        (first values))))
+
+(define (macro? x env)
+  (not (value? (get x env))))
+
+(define (expand sexp env)
+  (match sexp
+    ((list 'lambda (list args ...) body ...)
+     (list* 'lambda args (map (let ((new-env
+                                     (foldl (lambda (id old-env)
+                                              (extend id (value) old-env))
+                                            env
+                                            args)))
+                                (lambda (subexp) (expand subexp new-env)))
+                              body)))
+    ((list head args ...)
+     (if (and (symbol? head) (macro? head env))
+         (apply-macro (get head env) sexp)
+         (map (lambda (subexp) (expand subexp env)) sexp)))
+    (_ sexp)))
+
+(define env-with-let
+  (extend 'let
+          '(lambda (stx)
+             (match stx
+                    ((list 'let (list (list args vals) ...) body ...)
+                     `((lambda ,args ,@body) ,@vals))))
+          init-env))
+
+(define (apply-macro macro sexp)
+  (eval `(,macro ',sexp)))
+
+;; simple evaluator
+(struct closure (params body env))
+
+(define (my-eval sexp env)
+  (match sexp
+    ((? symbol?) (lookup sexp env))
+    ((? number?) sexp)
+    ((list 'lambda (list params ...) body ...)
+     (closure params body env))
+    ((list head tail ...)
+     (let* ((procedure (my-eval head env))
+            (closure (if (symbol? procedure)
+                         (get procedure env)
+                         procedure))
+            (args (map (lambda (sexp) (my-eval sexp env)) tail)))
+       (my-eval-closure closure args)))))
+
+(define (lookup id env)
+  (if (hash-has-key? env id)
+      (get id env)
+      (error 'lookup "undefined identifier: ~a in ~a" id env)))
+
+(define (my-eval-closure closure args)
+  (let ((params (closure-params closure))
+        (body (closure-body closure))
+        (env (closure-env closure)))
+    (let ((new-env (extend-list params args env)))
+      (last (map (lambda (sexp) (my-eval sexp new-env)) body)))))
