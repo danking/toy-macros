7802bc454c8f0cf5b77b3a340a0f4148d20e0aff
Daniel King <danking@ccs.neu.edu>
2011-02-16 16:48:36 -0500

    added new tests and tided up
    - added a real let macro tests
    - moved everything into sections

--- tests.rkt
+++ tests.rkt
@@ -2,15 +2,20 @@
 (require rackunit
          "main.rkt")
 
-(check-equal? (expand '(lambda (x) (+ x 3 4)) init-env)
-              '(lambda (x) (+ x 3 4)))
-(check-equal? (expand '(sqr 2)
+;; sanity
+(check-eq? (expand '(lambda (x) x) init-env) '(lambda (x) x))
+(check-eq? (expand '(lambda (x y) (x y))) init-env '(lambda (x y) (x y)))
+
+;; basic
+(check-eq? (expand '(sqr 2)
                       (extend 'sqr
                               '(lambda (sexp)
                                  `(* ,(second sexp) ,(second sexp)))
                               init-env))
               '(* 2 2))
-(check-equal? (expand '(let (x 3) (+ x x))
+
+;; simple let
+(check-eq? (expand '(let (x 3) (+ x x))
                       (extend 'let
                               '(lambda (sexp)
                                  `((lambda (,(first (second sexp)))
@@ -18,7 +23,9 @@
                                    ,(second (second sexp))))
                               init-env))
               '((lambda (x) (+ x x) 3)))
-(check-equal? (expand '(let ((x 3) (y 4))
+
+;; real let
+(check-eq? (expand '(let ((x 3) (y 4))
                          (* 2 (+ x y)))
                       (extend 'let
                               '(lambda (sexp)
