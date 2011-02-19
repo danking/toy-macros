460488025bb6723f7094ed7a18e7a0f6840943e4
Daniel King <danking@ccs.neu.edu>
2011-02-19 02:02:40 -0500

    handle function applications whose head isn't an identifier

--- renaming-macros.rkt
+++ renaming-macros.rkt
@@ -27,7 +27,7 @@
     ((? literal?) (values sexp renamed-env))
     ((? primitive-syntax?) (values sexp renamed-env))
     ((? symbol?) (rename&expand-symbol sexp renamed-env value-env))
-    ((list head tail ...)
+    ((list (and (? symbol?) head) tail ...)
      (let [(renamed-id (lookup-in-env head renamed-env))]
        (match (lookup-in-env renamed-id value-env)
          ((? lambda-stx?) (rename&expand-lambda renamed-id
@@ -40,7 +40,11 @@
          (macro           (rename&expand-macro macro
                                                sexp
                                                renamed-env
-                                               value-env)))))))
+                                               value-env)))))
+    ((list head tail ...)
+     (rename&expand/many-sexps sexp
+                               renamed-env
+                               value-env))))
 
 (define (rename&expand-symbol symbol renamed-env value-env)
   (let-values ([(id renamed-env) (rename-id symbol renamed-env)])
