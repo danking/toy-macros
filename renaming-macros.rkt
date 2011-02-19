47be4a3e62fd7ae19e23c133e33ce6e2c2777017
Daniel King <danking@ccs.neu.edu>
2011-02-18 20:06:09 -0500

    fixed a bunch of things related to renaming.

--- renaming-macros.rkt
+++ renaming-macros.rkt
@@ -1,104 +1,110 @@
 #lang racket
 
-(struct primitive-syntax ())
-(struct lambda-stx primitive-syntax ())
-(struct if-stx primitive-syntax ())
-(struct ref-stx primitive-syntax ())
+(provide (all-defined-out))
 
-(define empty-env '())
-(define initial-env
-  (extend-env/frame (make-frame `((if ,(if-stx))
-                                  (lambda ,(lambda-stx))))
-                    empty-env))
+(struct primitive-syntax () #:transparent)
+(struct lambda-stx primitive-syntax () #:transparent)
+(struct if-stx primitive-syntax () #:transparent)
+(struct ref-stx primitive-syntax (id) #:transparent)
 
 ;; used to bind non-macro identifiers
 (struct not-macro ())
 
-;; [AssocList X X] is [ListOf (list X X)]
 
-;; rename&expand : SExp [AssocList Symbol Symbol]
+;; SExp -> SExp [Environment Symbol]
+(define (rename&expand* sexp)
+  (rename&expand sexp empty-env initial-env))
+
+
+;; rename&expand : SExp
+;;                 [Environment Symbol]
 ;;                 [Environment (U Not-Macro SExp)]
-;;              -> SExp [AssocList Symbol Symbol]
+;;              -> SExp [Environment Symbol]
 ;; renames all the identifiers so that there is no shadowing.  expands macros
 ;; as it goes
-(define (rename&expand sexp renamed-ids env)
+(define (rename&expand sexp renamed-env env)
   (match sexp
-    ((? literal?) (values sexp rename-ids))
-    ((? primitive-syntax? (values sexp rename-ids)))
-    ((? symbol?) (rename-id sexp renamed-ids))
-    ((list 'lambda (args ...) body ...)
+    ((? literal?) (values sexp renamed-env))
+    ((? primitive-syntax?) (values sexp renamed-env))
+    ((? symbol?) (rename-id sexp renamed-env))
+    ((list 'lambda (list args ...) body ...)
      (if (lambda-stx? (lookup-in-env 'lambda env))
-         ()))
+         '...
+         '...))
     ((list head tail ...)
-     (rename&expand-list head tail renamed-ids env))))
+     (rename&expand-list head tail renamed-env env))))
 
-(define (rename&expand/many-sexps sexps renamed-ids env)
+(define (rename&expand/many-sexps sexps renamed-env env)
   (foldr (lambda (sexp result+ids)
            (let*-values
-               (((result renamed-ids) result+ids)
-                ((new-sexp new-ids) (rename&expand sexp renamed-ids env)))
+               (((result renamed-env) result+ids)
+                ((new-sexp new-ids) (rename&expand sexp renamed-env env)))
              (values (cons new-sexp result)
-                     (cons new-ids renamed-ids))))
-         (values '() renamed-ids)
+                     (cons new-ids renamed-env))))
+         (values '() renamed-env)
          sexps))
 
-(define (rename&expand-list head tail renamed-ids env)
+(define (rename&expand-list head tail renamed-env env)
   (let ((maybe-macro (lookup-in-env head env))
         (sexp (cons head tail)))
     (if (not-macro? maybe-macro)
         (rename&expand/many-sexps sexp
-                                  renamed-ids
+                                  renamed-env
                                   env)
         (rename&expand-macro maybe-macro
                              sexp
-                             renamed-ids
+                             renamed-env
                              env))))
 
-(define (rename&expand-macro macro sexp renamed-ids env)
+(define (rename&expand-macro macro sexp renamed-env env)
   (let-values
-      (((expanded-sexp new-renamed-ids) (expand macro sexp renamed-ids env)))
-    (rename&expand expanded-sexp renamed-ids env)))
+      (((expanded-sexp new-renamed-env) (expand macro sexp renamed-env env)))
+    (rename&expand expanded-sexp new-renamed-env env)))
 
-;; expand : SExp SExp [Environment (U Not-Macro SExp)]
-;;       -> SExp [Environment Symbol Symbol]
-(define (expand macro sexp renamed-ids env)
-  (let* ((*updated-renamed-ids* renamed-ids)
-         (update (lambda (new-renamed-ids)
-                   (set! *updated-renamed-ids* new-renamed-ids))))
-    (values (macro sexp (get-in-macro-renamer update))
-            *updated-renamed-ids*)))
+;; expand : SExp SExp [Environment Symbol] [Environment (U Not-Macro SExp)]
+;;       -> SExp [Environment Symbol]
+(define (expand macro sexp renamed-env env)
+  (let* ((*updated-renamed-env* renamed-env)
+         (update (lambda (new-renamed-env)
+                   (set! *updated-renamed-env* new-renamed-env))))
+    (values (apply-macro macro sexp (get-in-macro-renamer renamed-env update))
+            *updated-renamed-env*)))
 
-;; get-in-macro-renamer : [Environment Symbol Symbol] (Symbol Symbol -> #<void>)
+(define (apply-macro macro sexp renamer)
+  (macro sexp renamer))
+
+;; get-in-macro-renamer : [Environment Symbol] (Symbol Symbol -> #<void>)
 ;;                     -> Symbol
-;; takes the `renamed-ids' and produces a procedure which can be passed to a
+;; takes the `renamed-env' and produces a procedure which can be passed to a
 ;; macro and used to rename desired symbols.  This procedure will call
-;; `update' every time a new `renamed-ids' association list is created
-(define (get-in-macro-renamer renamed-ids update)
+;; `update' every time a new `renamed-env' association list is created
+(define (get-in-macro-renamer renamed-env update)
   (lambda (id)
-    (let-values (((new-id renamed-ids) (rename-id id renamed-ids)))
-      (update renamed-ids)
+    (let-values (((new-id renamed-env) (rename-id id renamed-env)))
+      (update renamed-env)
       new-id)))
-
-(define *gensym-count* 0)
 
 ;; rename-id : Symbol [ListOf (list Symbol Symbol)]
 ;;          -> Symbol [Environment Symbol Symbol]
-(define (rename-id id renamed-ids)
-  (if (bound-in-env? id renamed-ids)
-      (values (lookup-in-env id renamed-ids) renamed-ids)
-      (find-new-id id renamed-ids)))
+(define (rename-id id renamed-env)
+  (if (bound-in-env? id renamed-env)
+      (values (lookup-in-env id renamed-env) renamed-env)
+      (find-new-id id renamed-env)))
 
-;; find-new-id : Symbol [Environment Symbol Symbol] Number
+;; find-new-id : Symbol [Environment Symbol Symbol]
 ;;            -> Symbol [Environment Symbol Symbol]
 ;; finds a an id which hasn't been mapped to (renamed to) yet and adds it to
-;; the renamed-ids, returning the new id and the new renamed-ids
-(define (find-new-id id renamed-ids)
-  (let [(new-id (symbol-num 'g *gensym-count*))]
-    (values (ref-stx new-id) (extend-env/binding id new-id))))
+;; the renamed-env, returning the new id and the new renamed-env
+(define find-new-id
+  (let ((*gensym-count* 0))
+    (lambda (id renamed-env)
+      (let [(new-id (symbol-num 'g *gensym-count*))]
+        (set! *gensym-count* (add1 *gensym-count*))
+        (values (ref-stx new-id) (extend-env/binding id new-id renamed-env))))))
 
 (define (symbol-num id n)
   (string->symbol (string-append (symbol->string id)
-                                 (number->String n))))
+                                 (number->string n))))
 
 (define (literal? x)
   (or (number? x) (string? x)))
@@ -140,3 +146,10 @@
           (if value
               (second value)
               (loop (rest env)))))))
+
+(define empty-env '())
+(define initial-env
+  (extend-env/frame (make-frame `((if ,(if-stx))
+                                  (lambda ,(lambda-stx))))
+                    empty-env))
+
